43b81695d99f07a42332c891ef3b654bbd9630cd
Fahad Javed <Fahad_Javed008@yahoo.com>
2019-08-19 01:14:39 +0500

    Preparing the project for routing.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,34 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Posts from "./Posts/Posts";
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
-    }
-
     render() {
-        let posts = <p style={{ textAlign: 'center' }}>
-            Something went wrong while fetching the posts data!
-        </p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(
-                post => {
-                    return <Post
-                        key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        selected={() => this.postSelectedHandler(post.id)} />
-                }
-            );
-        }
-
         return (
             <div className="Blog">
                 <header>
@@ -43,32 +18,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <Posts />
             </div>
         );
-    }
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                // query the response data (posts in this case), transform it, and then store it in the component's state to cause a re-render
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(
-                    post => {
-                        return {
-                            ...post,
-                            author: 'Max'
-                        }
-                    }
-                );
-                this.setState({ posts: updatedPosts });
-            })
-            .catch(error => {
-                // console.log(error);
-                this.setState({error: true})
-            });
     }
 }
 
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>
+            Something went wrong while fetching the posts data!
+        </p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(
+                post => {
+                    return <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        selected={() => this.postSelectedHandler(post.id)} />
+                }
+            );
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        );
+    }
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
+                // query the response data (posts in this case), transform it, and then store it in the component's state to cause a re-render
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(
+                    post => {
+                        return {
+                            ...post,
+                            author: 'Max'
+                        }
+                    }
+                );
+                this.setState({ posts: updatedPosts });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+}
+
+export default Posts;
