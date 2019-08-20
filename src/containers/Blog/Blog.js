74d97e29586046d4529933cb39a978a518175424
Fahad Javed <Fahad_Javed008@yahoo.com>
2019-08-20 07:50:46 +0500

    Rendering components for different Routes.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import Posts from './Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
@@ -18,8 +19,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>}/>
-                <Route path="/" render={() => <h1>Home 2</h1>}/>
+                <Route path="/" exact component={Posts} />
             </div>
         );
     }
